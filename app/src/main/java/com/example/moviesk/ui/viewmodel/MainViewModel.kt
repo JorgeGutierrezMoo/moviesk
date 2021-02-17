8245693b9c2c1c2c732b79b6a207676890bedfcd
JorgeGutierrezMoo <39363872+JorgeGutierrezMoo@users.noreply.github.com>
2021-02-16 22:23:36 -0600

    Finished v1

---
+++ app/src/main/java/com/example/moviesk/ui/viewmodel/MainViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.moviesk.ui.viewmodel
+
+import androidx.lifecycle.*
+import com.example.moviesk.Coroutines
+import com.example.moviesk.data.model.Movie
+import com.example.moviesk.data.model.MovieDetail
+import com.example.moviesk.domain.Repo
+import com.example.moviesk.vo.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+
+class MainViewModel(private val repo: Repo): ViewModel() {
+    var pageIndexMovie: Int = 2
+    private lateinit var job:Job
+    private var _fetchMoviesList = MutableLiveData<Resource<List<Movie>>>()
+    val fetchMoviesList:LiveData<Resource<List<Movie>>>
+    get() = _fetchMoviesList
+
+    private val movieIdData = MutableLiveData<String>()
+    init {
+        refreshMovieList()
+    }
+    fun setMovieIdData(movie_id: String){
+        movieIdData.value = movie_id
+    }
+
+
+
+//    private val pageIndexData = MutableLiveData<Int>()
+//    init {
+//        setPageIndexMovieData(2)
+//    }
+//    fun setPageIndexMovieData(page_index: Int){
+//        pageIndexData.value = page_index
+//    }
+//------------------------------------------------------------------------------
+    fun refreshMovieList() {
+        job = Coroutines.ioThenMain({
+            repo.getMoviesList(pageIndexMovie)
+        },{
+            _fetchMoviesList.value = it
+        })
+    }
+
+    val fetchMovieDetail = movieIdData.distinctUntilChanged().switchMap { movieId ->
+        liveData(Dispatchers.IO){
+        emit(Resource.Loading())
+        try {
+            emit(repo.getMovieDetail(movieId))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+    } }
+}
